8ebb0ee9f2efbc7eba24539cc62e2ce0b7d4acfc
noahbelton29 <bobo9070xt@pm.me>
2026-04-03 20:19:57 +0100

    removed unnecessary breaks from token printer

--- src/token_printer.cpp
+++ src/token_printer.cpp
@@ -8,46 +8,32 @@ std::string tokenTypeName(TokenType type) {
   switch (type) {
   case TokenType::Number:
     return "Number";
-    break;
   case TokenType::Identifier:
     return "Identifier";
-    break;
   case TokenType::Var:
     return "Var";
-    break;
   case TokenType::Const:
     return "Const";
-    break;
   case TokenType::Plus:
     return "Plus";
-    break;
   case TokenType::Slash:
     return "Slash";
-    break;
   case TokenType::Star:
     return "Star";
-    break;
   case TokenType::Minus:
     return "Minus";
-    break;
   case TokenType::Equals:
     return "Equals";
-    break;
   case TokenType::Semicolon:
     return "Semicolon";
-    break;
   case TokenType::Colon:
     return "Colon";
-    break;
   case TokenType::Unknown:
     return "Unknown";
-    break;
   case TokenType::EndOfFile:
     return "EndOfFile";
-    break;
   default:
     return "Unknown token";
-    break;
   }
 }
 
